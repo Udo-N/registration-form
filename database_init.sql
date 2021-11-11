95ddf741026ce91f1a0fc9501c2c65e11af4db91
Udo-N <udoka404@gmail.com>
2021-11-11 05:13:51 -0500

    Initial files

---
+++ database_init.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS `DemoRegistration` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `DemoRegistration` ;
+
+-- Privileges
+-- (activate this statement if necessary)
+-- GRANT SELECT, INSERT, DELETE, UPDATE, ALTER ON `DemoRegistration`.* TO
+--  'pma'@localhost;
+
+CREATE TABLE IF NOT EXISTS `DemoRegistration`.`STUDENTS` (
+  `UMID` INT UNSIGNED NOT NULL,
+  `FName` VARCHAR(15) NOT NULL,
+  `LName` VARCHAR(15) NOT NULL,
+  `Project_Title` VARCHAR(45) NOT NULL,
+  `Email_Address` INT UNSIGNED NOT NULL,
+  `Phone_Number` INT NOT NULL,
+  `Time_Slot` INT NOT NULL,
+  PRIMARY KEY (`UMID`)
+)
+  COMMENT='LIST OF REGISTERED STUDENTS'
+  DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
